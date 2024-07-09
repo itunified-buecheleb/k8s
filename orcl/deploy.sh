2db3a6d35a2835dd1524e63ec98c0f428e5145f6
itunified-buecheleb <buecheleb@itunified.de>
2024-07-09 08:10:14 +0200

    fix: svault

--- orcl/deploy.sh
+++ orcl/deploy.sh
@@ -34,13 +34,26 @@ fi
 # Apply the YAML files in the specified namespace
  kubectl create secret docker-registry docker-hub-cred \
   --docker-server=https://index.docker.io/v1/ \
-  --docker-username=$(svault --getUsername --name hub_docker_buecheleb) \
-  --docker-password=$(svault --getPassword --name hub_docker_buecheleb) -n $NAMESPACE
+  --docker-username=$(cd ~/github/itunified-buecheleb/spass/ && node src/main --getUsername --name hub_docker_buecheleb) \
+  --docker-password=$(cd ~/github/itunified-buecheleb/spass/ && node src/main --getPassword --name hub_docker_buecheleb) -n $NAMESPACE
+
+sleep 5
 
 kubectl apply -f pv.yaml --namespace="$NAMESPACE"
 kubectl apply -f pvc.yaml --namespace="$NAMESPACE"
 kubectl apply -f deployment.yaml --namespace="$NAMESPACE"
 kubectl apply -f service.yaml --namespace="$NAMESPACE"
+
+# Check the status of the Oracle database pod
+#kubectl wait --for=condition=Ready pod -l app=oracle-database -n orcl --timeout=300s
+
+# Verify if the Oracle database pod is ready
+if [ $? -eq 0 ]; then
+    echo "Oracle database pod is ready."
+else
+    echo "Oracle database pod is not ready. Check the logs for more details."
+    exit 1
+fi
 
 echo "Deployment abgeschlossen in Namespace $NAMESPACE."
 
